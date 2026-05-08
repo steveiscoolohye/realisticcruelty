b9b4c4f9c64187ea327b7e97579cb15a28d018f8
steveiscoolohye <moskowitzsteve58@gmail.com>
2026-05-08 11:39:52 -0700

    Update BloodSplashParticle.java

--- src/main/java/com/xm666/realisticcruelty/particle/BloodSplashParticle.java
+++ src/main/java/com/xm666/realisticcruelty/particle/BloodSplashParticle.java
@@ -1,12 +1,10 @@
-package com.xm666.realisticcruelty.particle;
+package com.moskowitz.realisticcruelty.particle;
 
-import com.xm666.realisticcruelty.CruelConfig;
-import com.xm666.realisticcruelty.math.InverseFunction;
+import com.moskowitz.realisticcruelty.CruelConfig;
+import com.moskowitz.realisticcruelty.math.InverseFunction;
+import com.moskowitz.realisticcruelty.math.Process;
 import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.client.particle.ParticleProvider;
-import net.minecraft.client.particle.ParticleRenderType;
-import net.minecraft.client.particle.SpriteSet;
-import net.minecraft.client.particle.TextureSheetParticle;
+import net.minecraft.client.particle.*;
 import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
@@ -22,42 +20,49 @@ public class BloodSplashParticle extends TextureSheetParticle {
         this.lifetime = 20;
         this.quadSize = 0.05F;
         this.gravity = 1.0F;
+        // Random initial spray velocity
         this.xd = (Math.random() * 2 - 1) * 0.15D;
         this.yd = Math.random() * 0.2D + 0.1D;
         this.zd = (Math.random() * 2 - 1) * 0.15D;
     }
 
+    @Override
     public void tick() {
         super.tick();
         if (this.onGround) {
-            int i = this.lifetime + 1 - 5;
-            if (this.age < i) {
-                this.age = i;
-                float f = Mth.randomBetween(this.random, 0.3F, 1.0F) * CruelConfig.splashAmount.get();
+            int fadeStartAge = this.lifetime + 1 - 5;
+            if (this.age < fadeStartAge) {
+                this.age = fadeStartAge;
+                
+                // Fixed: Respect soundVolume config instead of splashAmount
+                float volume = Mth.randomBetween(this.random, 0.3F, 1.0F) * CruelConfig.soundVolume.get().floatValue();
                 SoundEvent soundevent = SoundEvents.BEEHIVE_DRIP;
-                this.level.playLocalSound(this.x, this.y, this.z, soundevent, SoundSource.BLOCKS, f, 1.0F, false);
+                this.level.playLocalSound(this.x, this.y, this.z, soundevent, SoundSource.BLOCKS, volume, 1.0F, false);
             }
         }
     }
 
+    @Override
     public float getQuadSize(float tick) {
-        float time = this.age + tick;
-        float[] size = {this.quadSize};
+        float time = (float)this.age + tick;
+        final float[] sizeArr = {this.quadSize};
+        
         Process.f(time, 5, this.lifetime + 1 - 5, 5,
-                (f) -> size[0] *= fadeIn.f(f),
-                () -> {
-                },
+                (f) -> sizeArr[0] *= (float) fadeIn.f(f),
+                () -> { },
                 (f) -> this.alpha = (float) fadeOut.f(f)
         );
-        return size[0];
+        return sizeArr[0];
     }
 
+    @Override
     public ParticleRenderType getRenderType() {
         return ParticleRenderType.PARTICLE_SHEET_TRANSLUCENT;
     }
 
     public record Provider(SpriteSet sprites) implements ParticleProvider<SimpleParticleType> {
-        public BloodSplashParticle createParticle(SimpleParticleType type, ClientLevel level, double x, double y, double z, double xd, double yd, double zd) {
+        @Override
+        public Particle createParticle(SimpleParticleType type, ClientLevel level, double x, double y, double z, double xd, double yd, double zd) {
             BloodSplashParticle particle = new BloodSplashParticle(level, x, y, z);
             particle.pickSprite(this.sprites);
             return particle;
