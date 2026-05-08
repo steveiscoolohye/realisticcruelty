625f33576f472f75a4856ff298ef18f271e6183b
steveiscoolohye <moskowitzsteve58@gmail.com>
2026-05-08 11:39:27 -0700

    Update BloodParticle.java

--- src/main/java/com/xm666/realisticcruelty/particle/BloodParticle.java
+++ src/main/java/com/xm666/realisticcruelty/particle/BloodParticle.java
@@ -1,13 +1,10 @@
-package com.xm666.realisticcruelty.particle;
+package com.moskowitz.realisticcruelty.particle;
 
-
-import com.xm666.realisticcruelty.CruelConfig;
-import com.xm666.realisticcruelty.math.InverseFunction;
+import com.moskowitz.realisticcruelty.CruelConfig;
+import com.moskowitz.realisticcruelty.math.InverseFunction;
+import com.moskowitz.realisticcruelty.math.Process; // Ensure this is updated to your package
 import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.client.particle.ParticleProvider;
-import net.minecraft.client.particle.ParticleRenderType;
-import net.minecraft.client.particle.SpriteSet;
-import net.minecraft.client.particle.TextureSheetParticle;
+import net.minecraft.client.particle.*;
 import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
@@ -26,42 +23,55 @@ public class BloodParticle extends TextureSheetParticle {
         this.quadSize = 0.1F;
     }
 
+    @Override
     public void tick() {
         super.tick();
         if (this.onGround) {
-            int i = this.lifetime + 1 - 10;
-            if (this.age < i) {
-                this.age = i;
+            int fadeStartAge = this.lifetime + 1 - 10;
+            if (this.age < fadeStartAge) {
+                this.age = fadeStartAge;
+                
+                // Trigger the Splat effect on the ground
                 if (CruelConfig.splatEnable.get()) {
-                    this.level.addParticle(ModParticle.BLOOD_SPLAT.get(), this.x, this.y + 0.1D, this.z, 0.0D, 0.0D, 0.0D);
+                    this.level.addParticle(ModParticle.BLOOD_SPLAT.get(), this.x, this.y + 0.05D, this.z, 0.0D, 0.0D, 0.0D);
                 }
-                for (int j = CruelConfig.splashAmount.get(); j-- > 0; ) {
+                
+                // Trigger small splashes
+                int splashCount = CruelConfig.splashAmount.get();
+                for (int j = 0; j < splashCount; j++) {
                     this.level.addParticle(ModParticle.BLOOD_SPLASH.get(), this.x, this.y, this.z, 0.0D, 0.0D, 0.0D);
                 }
-                float f = Mth.randomBetween(this.random, 0.3F, 1.0F) * CruelConfig.soundVolume.get();
-                SoundEvent soundevent = SoundEvents.BEEHIVE_DRIP;
-                this.level.playLocalSound(this.x, this.y, this.z, soundevent, SoundSource.BLOCKS, f, 1.0F, false);
+                
+                // Play the "drip" sound realistically
+                float volume = Mth.randomBetween(this.random, 0.3F, 1.0F) * CruelConfig.soundVolume.get().floatValue();
+                SoundEvent dripSound = SoundEvents.BEEHIVE_DRIP;
+                this.level.playLocalSound(this.x, this.y, this.z, dripSound, SoundSource.BLOCKS, volume, 1.0F, false);
             }
         }
     }
 
+    @Override
     public float getQuadSize(float tick) {
-        float time = this.age + tick;
-        float[] size = {this.quadSize};
+        float time = (float)this.age + tick;
+        final float[] currentSize = {this.quadSize};
+        
+        // This helper handles the smooth transition from spray to splat
         Process.f(time, 5, this.lifetime + 1 - 10, 10,
                 (f) -> this.alpha = (float) fadeIn.f(f),
                 () -> this.alpha = 1.0F,
-                (f) -> size[0] *= fadeOut.f(f)
+                (f) -> currentSize[0] *= (float) fadeOut.f(f)
         );
-        return size[0];
+        return currentSize[0];
     }
 
+    @Override
     public ParticleRenderType getRenderType() {
         return ParticleRenderType.PARTICLE_SHEET_TRANSLUCENT;
     }
 
     public record Provider(SpriteSet sprites) implements ParticleProvider<SimpleParticleType> {
-        public BloodParticle createParticle(SimpleParticleType type, ClientLevel level, double x, double y, double z, double xd, double yd, double zd) {
+        @Override
+        public Particle createParticle(SimpleParticleType type, ClientLevel level, double x, double y, double z, double xd, double yd, double zd) {
             BloodParticle particle = new BloodParticle(level, x, y, z);
             particle.pickSprite(this.sprites);
             particle.setParticleSpeed(xd, yd, zd);
