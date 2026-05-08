1e96055632ecfb4a209e701a2a6b577355898f77
steveiscoolohye <moskowitzsteve58@gmail.com>
2026-05-08 11:40:50 -0700

    Update ModParticle.java

--- src/main/java/com/xm666/realisticcruelty/particle/ModParticle.java
+++ src/main/java/com/xm666/realisticcruelty/particle/ModParticle.java
@@ -1,26 +1,36 @@
-package com.xm666.realisticcruelty.particle;
+package com.moskowitz.realisticcruelty.particle;
 
-import com.xm666.realisticcruelty.CruelMod;
+import com.moskowitz.realisticcruelty.CruelMod;
 import net.minecraft.core.particles.ParticleType;
 import net.minecraft.core.particles.SimpleParticleType;
-import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import net.minecraft.core.registries.Registries;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.RegisterParticleProvidersEvent;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
 
+@EventBusSubscriber(modid = CruelMod.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 public class ModParticle {
-    public static final DeferredRegister<ParticleType<?>> REGISTER = DeferredRegister.create(ForgeRegistries.PARTICLE_TYPES, CruelMod.MOD_ID);
-    public static final RegistryObject<SimpleParticleType> BLOOD = REGISTER.register("blood", () -> new SimpleParticleType(true));
-    public static final RegistryObject<SimpleParticleType> BLOOD_SPLAT = REGISTER.register("blood_splat", () -> new SimpleParticleType(true));
-    public static final RegistryObject<SimpleParticleType> BLOOD_FOG = REGISTER.register("blood_fog", () -> new SimpleParticleType(true));
-    public static final RegistryObject<SimpleParticleType> BLOOD_SPLASH = REGISTER.register("blood_splash", () -> new SimpleParticleType(true));
+    // 1.21.1 uses Registries.PARTICLE_TYPE and DeferredHolder instead of RegistryObject
+    public static final DeferredRegister<ParticleType<?>> REGISTER = DeferredRegister.create(Registries.PARTICLE_TYPE, CruelMod.MOD_ID);
+
+    public static final DeferredHolder<ParticleType<?>, SimpleParticleType> BLOOD = REGISTER.register("blood", () -> new SimpleParticleType(true));
+    public static final DeferredHolder<ParticleType<?>, SimpleParticleType> BLOOD_SPLAT = REGISTER.register("blood_splat", () -> new SimpleParticleType(true));
+    public static final DeferredHolder<ParticleType<?>, SimpleParticleType> BLOOD_FOG = REGISTER.register("blood_fog", () -> new SimpleParticleType(true));
+    public static final DeferredHolder<ParticleType<?>, SimpleParticleType> BLOOD_SPLASH = REGISTER.register("blood_splash", () -> new SimpleParticleType(true));
+
+    // Helper to register the DeferredRegister to the mod bus
+    public static void register(IEventBus eventBus) {
+        REGISTER.register(eventBus);
+    }
 
     @SubscribeEvent
     public static void onRegisterParticleProviders(RegisterParticleProvidersEvent event) {
-        event.register(ModParticle.BLOOD.get(), BloodParticle.Provider::new);
-        event.register(ModParticle.BLOOD_SPLAT.get(), BloodSplatParticle.Provider::new);
-        event.register(ModParticle.BLOOD_FOG.get(), BloodFogParticle.Provider::new);
-        event.register(ModParticle.BLOOD_SPLASH.get(), BloodSplashParticle.Provider::new);
+        event.registerSpriteSet(ModParticle.BLOOD.get(), BloodParticle.Provider::new);
+        event.registerSpriteSet(ModParticle.BLOOD_SPLAT.get(), BloodSplatParticle.Provider::new);
+        event.registerSpriteSet(ModParticle.BLOOD_FOG.get(), BloodFogParticle.Provider::new);
+        event.registerSpriteSet(ModParticle.BLOOD_SPLASH.get(), BloodSplashParticle.Provider::new);
     }
 }
