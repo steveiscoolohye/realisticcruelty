abe329571398e8849476244f38687fece613033b
steveiscoolohye <moskowitzsteve58@gmail.com>
2026-05-08 11:41:38 -0700

    Update InverseFunction.java

--- src/main/java/com/xm666/realisticcruelty/math/InverseFunction.java
+++ src/main/java/com/xm666/realisticcruelty/math/InverseFunction.java
@@ -1,5 +1,10 @@
-package com.xm666.realisticcruelty.math;
+package com.moskowitz.realisticcruelty.math;
 
+/**
+ * Handles the easing curves for particle size and transparency.
+ * This creates non-linear movement (hyperbolic functions) to make 
+ * blood spray and dissipation look cinematic rather than robotic.
+ */
 public class InverseFunction {
     public double offsetX, width, offsetY;
 
@@ -10,8 +15,10 @@ public class InverseFunction {
         this.offsetX = y / (1 - x);
         this.width = (1 - this.offsetX) / x;
         this.offsetY = -1 / (this.offsetX + this.width);
+        
         double shrink = 1 / this.offsetX + this.offsetY;
         this.shrink(shrink);
+        
         if (reversed) {
             offsetX += width;
             width = -width;
@@ -24,6 +31,11 @@ public class InverseFunction {
         this.offsetY /= shrink;
     }
 
+    /**
+     * The core curve function used by particles in getQuadSize.
+     * @param x The current progress (usually 0.0 to 1.0)
+     * @return The eased value
+     */
     public double f(double x) {
         return 1 / (this.offsetX + this.width * x) + this.offsetY;
     }
